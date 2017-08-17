07a69fe995b48c8e9ce133701edfa92ce69b4a39
leojojo <setsunal.fujita@gmail.com>
2017-08-17 10:32:05 +0900

    craft packet with flags RST+ACK

--- challenge05/main.c
+++ challenge05/main.c
@@ -48,8 +48,18 @@
 #include <rte_hexdump.h>
 #include <rte_log.h>
 #include <rte_mbuf.h>
+#include <rte_ether.h>
+#include <rte_ip.h>
+#include <rte_ethdev.h>
 
 #include "protocol.h"
+
+#define FIN (0x01 << 0)
+#define SYN (0x01 << 1)
+#define RST (0x01 << 2)
+#define PSH (0x01 << 3)
+#define ACK (0x01 << 4)
+#define URG (0x01 << 5)
 
 static struct rte_mbuf* get_pkt(void)
 {
@@ -94,29 +104,77 @@ static struct rte_mbuf* get_pkt(void)
 static bool is_domain(uint8_t * ptr){
   printf("\n=====HTTP=====\n");
   const char httpget[] = "GET / HTTP/1.1\r\nHost: dpdk.ninja\r\n";
-  if(memcmp(ptr, httpget, sizeof(httpget)-1) == 0){
-    printf("dpdkninja\n");
-  } else{
-    rte_hexdump(stdout, "hoge", ptr, sizeof(httpget));
-    rte_hexdump(stdout, "hoge", httpget, sizeof(httpget));
-  }
+  return memcmp(ptr, httpget, sizeof(httpget)-1) == 0;
 }
 
-static void send_rst_ack(struct rte_mbuf* m, uint8_t * pnt){
+static void send_rst_ack(struct rte_mbuf* m,
+    uint8_t srcip[4], uint8_t dstip[4],
+    uint16_t srcport, uint16_t dstport,
+    uint32_t seq, uint32_t ack){
+
+  struct ether_hdr *eth;
+  struct ip4_hdr *ip;
+  struct tcp_hdr *tcp;
+  size_t pkt_size;
+
+  // ==========CRAFTING PACKET==========
+  // -----ethernet-----
+  eth = rte_pktmbuf_mtod(m, struct ether_hdr *);  // points to start of the data in the mbuf, return type
+  rte_eth_macaddr_get(0, &eth->s_addr); // port, macAddr of Eth device
+  memset(&eth->d_addr, 0xFF, ETHER_ADDR_LEN); // FF:FF:FF:FF:FF
+  eth->ether_type = rte_cpu_to_be_16(ETHER_TYPE_IPv4); // CPU's endian -> big endian
+
+  // -----ip-----
+  ip = (struct ip4_hdr *)(eth+1);
+  ip->version_ihl = 0x45;
+  ip->tos = 0x00;
+  ip->totlen = rte_cpu_to_be_16(sizeof(struct ip4_hdr) + sizeof(struct tcp_hdr));
+  ip->id = rte_cpu_to_be_16(0xbeef);
+  ip->flag_off = rte_cpu_to_be_16((0x02 << 13) & 0x00); // 0x02 and 0x00 -> 010 0000000000000(13 bits)
+  ip->ttl = 0x40;
+  ip->proto = 0x06;
+  ip->checksum = 0;
+  for(int i = 0; i < 4; i++){
+    ip->src[i] = srcip[i];
+    ip->dst[i] = dstip[i];
+  }
+
+  // -----tcp-----
+  tcp = (struct tcp_hdr *)((uint8_t *)ip + (ip->version_ihl & 0x0f)* 4);
+  tcp->src_port = rte_cpu_to_be_16(srcport);
+  tcp->dst_port = rte_cpu_to_be_16(dstport);
+  tcp->sent_seq = rte_cpu_to_be_32(seq);
+  tcp->recv_ack = rte_cpu_to_be_32(ack);
+  tcp->data_off = sizeof(struct tcp_hdr);
+  tcp->tcp_flags = RST | ACK;
+  tcp->rx_win = rte_cpu_to_be_16(4000);
+  tcp->cksum = 0;
+  tcp->tcp_urp = rte_cpu_to_be_16(0);
+
+  pkt_size = sizeof(struct ether_hdr) + sizeof(struct ip4_hdr) + sizeof(struct tcp_hdr);
+  m->data_len = pkt_size;
+  m->pkt_len = pkt_size;
+  // ==========CRAFTING PACKET==========
+
+  rte_hexdump(stdout, "hoge", rte_pktmbuf_mtod(m, struct ether_hdr *), m->pkt_len);
+  //rte_eth_tx_burst(0, 0, &m, 1);  // port, queue, pointer to retrieved packets are stored in rte_mbuf, MAX packets to retrieve
 }
 
 static void analyze_packet(struct rte_mbuf* m)
 {
-  uint8_t* ethip_offset;
   uint8_t* pkt = rte_pktmbuf_mtod(m, uint8_t*);
   pkt += analyze_eth(pkt);
   pkt += analyze_ip(pkt);
-  ethip_offset = pkt;
   pkt += analyze_tcp(pkt);
   if(is_domain(pkt)){
-    send_rst_ack(m, ethip_offset);
+    printf("DPDKNINJA!!!!!\n");
+    uint8_t dummyip[4] = {0xaa,0xbb,0xcc,0xdd};
+    uint8_t dummyport = 0xee;
+    send_rst_ack(m, dummyip, dummyip, dummyport, dummyport, 0xabcdabcd, 0xabcdabcd);
   }
-  //size_t len = m->pkt_len;
+  else {
+    printf("notdpdkninja\n");
+  }
   //rte_hexdump(stdout, "Packet-Hexdump", pkt, len);
 }
 
